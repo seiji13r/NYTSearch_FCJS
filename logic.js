94a4d8fb239500ff177babb1f8ee2f1fc97599e3
Seiji Osada <seiji13r@outlook.com>
2018-09-22 14:25:25 -0500

    back to http

--- logic.js
+++ logic.js
@@ -20,7 +20,7 @@ $("#search-btn").on("click", function(event){
     console.log(termsNumber);
 
     //apiURL="http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+search+"&api-key=ec88054b4a7d464993b91f6eb54fba8f";
-    apiURL="https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+search+"&begin_date="+beginDate+"0101&end_date="+endDate+"0101&api-key=ec88054b4a7d464993b91f6eb54fba8f";
+    apiURL="http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+search+"&begin_date="+beginDate+"0101&end_date="+endDate+"0101&api-key=ec88054b4a7d464993b91f6eb54fba8f";
     $.ajax({
         url: apiURL,
         method: "GET"
