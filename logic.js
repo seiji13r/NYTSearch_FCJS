3606fbc29d0c727513609b6f9f35ee53a2dd96a5
Seiji Osada <seiji13r@outlook.com>
2018-09-22 15:08:32 -0500

    https added again

--- logic.js
+++ logic.js
@@ -1,49 +1,41 @@
-var apiURL="";
-
-var beginDate="";
-var endDate="";
-var search="";
-var termsNumber=0;
+var apiURL = "";
+var beginDate = "";
+var endDate = "";
+var search = "";
+var termsNumber = 0;
 
 
 $("#search-btn").on("click", function(event){
     event.preventDefault();
-    console.log("entra al click");
+    // console.log("entra al click");
     search=$("#search-term").val();
-    console.log(search);
+    // console.log(search);
     beginDate=$("#start-year").val();
-    console.log(beginDate);
+    // console.log(beginDate);
     endDate=$("#end-year").val();
-    console.log(endDate);
-
+    // console.log(endDate);
     termsNumber=$("#num-records option:selected" ).text();
-    console.log(termsNumber);
+    // console.log(termsNumber);
 
-    //apiURL="http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+search+"&api-key=ec88054b4a7d464993b91f6eb54fba8f";
-    apiURL="http://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+search+"&begin_date="+beginDate+"0101&end_date="+endDate+"0101&api-key=ec88054b4a7d464993b91f6eb54fba8f";
+    apiURL="https://api.nytimes.com/svc/search/v2/articlesearch.json?fq="+search+"&begin_date="+beginDate+"0101&end_date="+endDate+"0101&api-key=ec88054b4a7d464993b91f6eb54fba8f";
+
     $.ajax({
         url: apiURL,
         method: "GET"
-      }).then(function(result) {
-        console.log(result);
+    }).then(function(result) {
+        // console.log(result);
 
-        
-          
+        for(var i=0;i<termsNumber;i++){
+        var title=result.response.docs[i].headline.main;
+        var articleURL=result.response.docs[i].web_url;
+        var link=$("<a>");
+        link.attr("href", articleURL);
+        link.append(title);
 
-      for(var i=0;i<termsNumber;i++){
-      var title=result.response.docs[i].headline.main;
-      var articleURL=result.response.docs[i].web_url;
-      var link=$("<a>");
-      link.attr("href", articleURL);
-      link.append(title);
-
-      $("#search-results").append(link);
-      $("#search-results").append("<br>");
-      
-    
-    }
-});
-
+        $("#search-results").append(link);
+        $("#search-results").append("<br>");
+        }
+    });
 });
 
 
